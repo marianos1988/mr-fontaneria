d61ac7763776583f6839f6b99af5786e2f171739
Mariano SY <138610830+marianos1988@users.noreply.github.com>
2024-03-27 17:28:29 -0300

    Armado de nuevo Carousel de imagenes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 const toggleMenu = () => document.body.classList.toggle("open");
 
 document.querySelector(".burger").addEventListener("click", toggleMenu);
+
+
+Splitting();
+
+setTimeout(() => {
+  document.querySelector(".card-ga").focus();
+}, 1500);
