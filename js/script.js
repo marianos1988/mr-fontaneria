def514ddac8aeec2000dcd236e2693441570c82a
Mariano SY <138610830+marianos1988@users.noreply.github.com>
2024-03-28 09:19:22 -0300

    Correccion de interfaz carousel

--- js/script.js
+++ js/script.js
@@ -3,8 +3,3 @@ const toggleMenu = () => document.body.classList.toggle("open");
 document.querySelector(".burger").addEventListener("click", toggleMenu);
 
 
-Splitting();
-
-setTimeout(() => {
-  document.querySelector(".card-ga").focus();
-}, 1500);
