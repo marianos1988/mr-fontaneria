9e61919531852793676ad790482bb15bb17dc8ee
Mariano SY <138610830+marianos1988@users.noreply.github.com>
2024-04-03 16:36:55 -0300

    Agregar Spinner en las fotos

--- js/script.js
+++ js/script.js
@@ -2,4 +2,15 @@ const toggleMenu = () => document.body.classList.toggle("open");
 
 document.querySelector(".burger").addEventListener("click", toggleMenu);
 
+  //Load Photos Gallery
+  const loadPhotos = (quanPhotos) => {
 
+    for(let x=1;x<=quanPhotos;x++) {
+      document.querySelector(`.image-gallery-${x}`).addEventListener("load",()=>{
+        document.querySelector(`.spinner-image-${x}`).innerHTML=``;
+      });
+    }
+}
+
+// Ejecuta spinner
+loadPhotos(31);
